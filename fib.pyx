b813ce9efbb3b186c706d4073bff3471863ad32a
Sanek619 <79252884+Sanek619@users.noreply.github.com>
2021-06-26 16:30:54 +0300

    Add files via upload

---
+++ fib.pyx
@@ -0,0 +1,19 @@
+import time
+
+
+t_start = time.perf_counter()
+
+cdef long long n = 10
+cdef long long fib1 = 1
+cdef long long fib2 = 1
+cdef long long i = 0
+
+while i < n - 2:
+    current_sum = fib1 + fib2
+    fib1 = fib2
+    fib2 = current_sum
+    i = i + 1
+print(fib2)
+
+all_time = time.perf_counter() - t_start
+print('Time: {:.2f}s'.format(all_time))
