d0e188609ac0c1685751b910dce472682ec289f3
Sanek619 <79252884+Sanek619@users.noreply.github.com>
2021-06-26 16:28:40 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-directives = {'language_level': 3}
-setup(
-    ext_modules=cythonize('fib.pyx'),
-    zip_safe=False,
-    
-)
